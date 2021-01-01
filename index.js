0fd1cb55e8f7b5f903f83588eaa1f669fa3920a7
trinhamcity1 <trinhamcity1@gmail.com>
2021-01-01 13:31:31 -0600

    create user

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 //get all users
 app.get('/get', (req, res) =>{
-  const sql = 'SELECT * FROM Song.users where user_id =1;';
+  const sql = "SELECT * FROM Song.users where user_id =1;";
   const query = db.query(sql, (err, results) => {
       if(err) throw err;
       console.log(results);
@@ -24,8 +24,35 @@ app.get('/get', (req, res) =>{
       
   });
 });
-//get user=1
 
+
+//get user=1
+//get user
+app.get('/login', async (req, res) =>{
+  const sql = `SELECT * FROM Song.users where username = '${req.body.username}' and password = '${req.body.password}';`;
+  const query = await db.query(sql, (err, results) => {
+      if(err) throw err;
+      console.log(results);
+      res.status(200).json({
+          status:"yessire",
+          results
+      });
+      
+  });
+});
+//create user
+post.get('/signup', async (req, res) =>{
+  const sql = `INSERT INTO Song.users (username, password, puser) VALUES ( '${req.body.username}, '${req.body.password}', 1)  ;`;
+  const query2 = await db.query(sql, (err, results) => {
+      if(err) throw err;
+      console.log(results);
+      res.status(200).json({
+          status:"yessire",
+          results
+      });
+      
+  });
+});
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
