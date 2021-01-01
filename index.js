35a3325b37fb2aec09cf5b471f1c7f102ea52f72
JLopezz3 <lopezjerel@gmail.com>
2021-01-01 14:38:07 -0600

    setup Register and Login api

--- index.js
+++ index.js
@@ -1,58 +1,82 @@
 const express = require("express");
 const db = require("./db/db");
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const PORT = 3001;
 const morgan = require("morgan");
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//Creating an account
+app.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-//get all users
-app.get('/get', (req, res) =>{
-  const sql = "SELECT * FROM Song.users where user_id =1;";
-  const query = db.query(sql, (err, results) => {
-      if(err) throw err;
-      console.log(results);
-      res.status(200).json({
-          status:"yessire",
-          results
-      });
-      
-  });
+  db.query(
+    "INSERT INTO Song.users (username, password) VALUES (?,?)",
+    [username, password],
+    (err, result) => {
+      console.log(err);
+    }
+  );
 });
 
+//logging into an account
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query(
+    "SELECT * FROM Song.users WHERE username = ? AND password = ?",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.send({ err: err });
+        console.log({ err: err });
+      }
+
+      if (result.length > 0) {
+        res.send(result);
+        console.log(result);
+      } else {
+        res.send({ message: "Wrong username/password!" });
+        console.log({ message: "Wrong username/password!" });
+      }
+    }
+  );
+});
+
+// get all users
+app.get("/get", (req, res) => {
+  const sql = "SELECT * FROM Song.users where user_id =1;";
+  const query = db.query(sql, (err, results) => {
+    if (err) throw err;
+    console.log(results);
+    res.status(200).json({
+      status: "yessire",
+      results,
+    });
+  });
+});
 
 //get user=1
 //get user
-app.get('/login', async (req, res) =>{
-  const sql = `SELECT * FROM Song.users where username = '${req.body.username}' and password = '${req.body.password}';`;
-  const query = await db.query(sql, (err, results) => {
-      if(err) throw err;
-      console.log(results);
-      res.status(200).json({
-          status:"yessire",
-          results
-      });
-      
-  });
-});
-//create user
-post.get('/signup', async (req, res) =>{
-  const sql = `INSERT INTO Song.users (username, password, puser) VALUES ( '${req.body.username}, '${req.body.password}', 1)  ;`;
-  const query2 = await db.query(sql, (err, results) => {
-      if(err) throw err;
-      console.log(results);
-      res.status(200).json({
-          status:"yessire",
-          results
-      });
-      
-  });
-});
+// app.get('/login', async (req, res) =>{
+//   const sql = `SELECT * FROM Song.users where username = '${req.body.username}' and password = '${req.body.password}';`;
+//   const query = await db.query(sql, (err, results) => {
+//       if(err) throw err;
+//       console.log(results);
+//       res.status(200).json({
+//           status:"yessire",
+//           results
+//       });
+
+//   });
+// });
+
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
